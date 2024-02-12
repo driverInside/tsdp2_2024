4d6626d249f0d2885e766e1709fda25c1606ccfb
Hiram E. Perez <hiram.eps@gmail.com>
2024-02-12 07:42:58 -0600

    print list

--- ex1/node.js
+++ ex1/node.js
@@ -7,9 +7,32 @@ class Node {
 
 const one = new Node(5);
 const two = new Node(3);
-const three = new Node(42);
+const three = new Node(7);
+const four = new Node(42);
 
 one.next = two;
 two.next = three;
+three.next = four;
 
-// 5 -> 3 -> 42 ->
+/**
+ * printList
+ * 
+ * @param {Node} head The head node of the list
+ * @returns {string} The result string
+ */
+function printList(head) {
+  let aux = head;
+  let str = '';
+
+  str = aux.value + ' -> ';
+
+  while (aux.next !== null) {
+    aux = aux.next;
+    str = str + aux.value + ' -> ';
+  }
+  return str
+}
+
+console.log(printList(one));
+
+// 5 -> 3 -> 7 ->
