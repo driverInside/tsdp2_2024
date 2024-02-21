8c329ceef45870b9fdf0982c6db5348faccb98bf
Hiram E. Perez <hiram.eps@gmail.com>
2024-02-21 08:27:53 -0600

    test

---
+++ ex2/coordinates.js
@@ -0,0 +1,18 @@
+module.exports = function coordinates(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const result = [];
+  
+  // arr.forEach((coordinate) => {
+  //   console.log(coordinate);
+  // });
+  
+  
+  // for (let i = 0; i < arr.length; i++) {
+  //   console.log(arr[i]);
+  // }
+
+  return result;
+}
---
+++ ex2/coordinates.test.js
@@ -0,0 +1,23 @@
+const coordinates = require('./coordinates');
+
+const list = [
+  [5, 2], // 1
+  [-3, 9],
+  [-1, -1], // 3
+  [5, -3],
+  [0, 1],
+  [-8, -6],  // 3
+];
+
+describe('Coordinates', () => {
+  it('should be defined', () => {
+    expect(coordinates).toBeDefined();
+  });
+  it('should return an empty array', () => {
+    expect(coordinates([])).toStrictEqual([]);
+  });
+
+  it('should return an array with two elements', () => {
+    expect(coordinates(list)).toHaveLength(3);
+  });
+});
