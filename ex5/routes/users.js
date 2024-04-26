ff6a47d3286908fa51928686f08bfd9209ea8ff1
Hiram E. Perez <hiram.eps@gmail.com>
2024-04-26 08:53:37 -0500

    post method

--- ex5/routes/users.js
+++ ex5/routes/users.js
@@ -2,11 +2,28 @@ const express = require('express');
 const router = express.Router();
 const UserModel = require('../models/users');
 
-router.get('/', async (_, res) => {
-  const users = await UserModel.find({
-    lastname: "Pérez"
-  }).limit(1);
+router.get('/', async (req, res) => {
+  const query = req.query;
+  const limit = query.limit;
+
+  const users = await UserModel.find({})
+    .limit(limit);
+
   res.send(users);
+});
+
+
+router.post('/', async (req, res) => {
+  const user = req.body;
+
+  const newUser = new UserModel(user);
+
+  await newUser.save();
+
+  res.send({
+    message: "soy el método post",
+    user:newUser
+  });
 });
 
 module.exports = router;
